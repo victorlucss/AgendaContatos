eac397f6cd3d4593a34f3e83eb6d3dbd50224e97
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-16 13:14:45 -0300

    Activity de exibição de contatos

---
+++ app/src/main/java/com/br/agendacontatos/views/ContactsActivity.java
@@ -0,0 +1,16 @@
+package com.br.agendacontatos.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.agendacontatos.R;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
