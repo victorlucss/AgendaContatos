b1f3b38c1df7d73ba20138ca1f63026c195b6683
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-17 08:41:49 -0300

    Correção em Login Activity - redirecionamento para tela de login

--- app/src/main/java/com/br/agendacontatos/views/LoginActivity.java
+++ app/src/main/java/com/br/agendacontatos/views/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import android.widget.Toast;
 import com.br.agendacontatos.R;
 import com.br.agendacontatos.models.UserModel;
 import com.br.agendacontatos.viewmodels.LoginViewModel;
+
+import javax.annotation.Nullable;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,10 +34,14 @@ public class LoginActivity extends AppCompatActivity {
 
         loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
 
+        final Intent contacts = new Intent(this, ContactsActivity.class);
+
         loginViewModel.getCurrentUser().observe(this, new Observer<UserModel>() {
             @Override
-            public void onChanged(UserModel userModel) {
-                Toast.makeText(LoginActivity.this, "um teste bem realizado e funcional :D", Toast.LENGTH_SHORT).show();
+            public void onChanged(@Nullable UserModel userModel) {
+                contacts.putExtra("phone", phoneEditText.getText().toString());
+                finish();
+                startActivity(contacts);
             }
         });
 
