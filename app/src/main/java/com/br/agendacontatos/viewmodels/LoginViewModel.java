45d0c3aae0b6d75fd87313f15cbb15ea995e5850
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-17 08:50:21 -0300

    Correção em LoginViewModel para se adequar às correções de seu repositório

--- app/src/main/java/com/br/agendacontatos/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/br/agendacontatos/viewmodels/LoginViewModel.java
@@ -9,20 +9,18 @@ import com.br.agendacontatos.repositories.LoginRepository;
 
 public class LoginViewModel extends ViewModel {
 
+    private LoginRepository repository = LoginRepository.getInstance();
     private MutableLiveData<UserModel> currentUser = new MutableLiveData<>();
-
-    private LoginRepository repository;
 
     public LiveData<UserModel> getCurrentUser() {
         return currentUser;
     }
 
     public void login(String phone) {
-
         UserModel user = new UserModel(phone);
         repository.login(user);
 
-        this.currentUser.postValue(repository.getUser().getValue());
+        this.currentUser.postValue(repository.getUser());
     }
 
     public void logout(){
