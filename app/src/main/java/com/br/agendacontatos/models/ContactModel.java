77809f5fce04fd289c86025eff61f9b5d660dccc
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-17 08:43:12 -0300

    Atualização de modelo de contato

--- app/src/main/java/com/br/agendacontatos/models/ContactModel.java
+++ app/src/main/java/com/br/agendacontatos/models/ContactModel.java
@@ -1,6 +1,51 @@
 package com.br.agendacontatos.models;
 
-public class ContactModel {
+import java.io.Serializable;
 
+public class ContactModel implements Serializable {
 
+    private String name;
+
+    private String phone;
+
+    private String address;
+
+    public ContactModel(String name, String phone, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactModel{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
