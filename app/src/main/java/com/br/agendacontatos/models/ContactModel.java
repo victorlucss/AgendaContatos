9f5dd8a0656d697748b34636f0e68e3a65887947
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-17 18:58:07 -0300

    Atualização de ContactModel para inclusão de ChatId

--- app/src/main/java/com/br/agendacontatos/models/ContactModel.java
+++ app/src/main/java/com/br/agendacontatos/models/ContactModel.java
@@ -5,15 +5,15 @@ import java.io.Serializable;
 public class ContactModel implements Serializable {
 
     private String name;
-
     private String phone;
-
     private String address;
+    private String chatId;
 
-    public ContactModel(String name, String phone, String address) {
+    public ContactModel(String name, String phone, String address, String chatId) {
         this.name = name;
         this.phone = phone;
         this.address = address;
+        this.chatId = chatId;
     }
 
     public String getName() {
@@ -40,12 +40,21 @@ public class ContactModel implements Serializable {
         this.address = address;
     }
 
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
     @Override
     public String toString() {
         return "ContactModel{" +
                 "name='" + name + '\'' +
                 ", phone='" + phone + '\'' +
                 ", address='" + address + '\'' +
+                ", chatId='" + chatId + '\'' +
                 '}';
     }
 }
