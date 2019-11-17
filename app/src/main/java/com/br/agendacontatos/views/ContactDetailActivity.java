c44cadaae6287bd2d6f36220234b99e3087a0f86
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-17 19:05:03 -0300

    Botão para ir para o Chat na tela de Detalhes

--- app/src/main/java/com/br/agendacontatos/views/ContactDetailActivity.java
+++ app/src/main/java/com/br/agendacontatos/views/ContactDetailActivity.java
@@ -2,7 +2,10 @@ package com.br.agendacontatos.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.br.agendacontatos.R;
@@ -15,6 +18,7 @@ public class ContactDetailActivity extends AppCompatActivity {
     private TextView nameTextView;
     private TextView phoneTextView;
     private TextView addressTextView;
+    private Button gochatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +30,22 @@ public class ContactDetailActivity extends AppCompatActivity {
         nameTextView = findViewById(R.id.text_name);
         phoneTextView = findViewById(R.id.text_phone);
         addressTextView = findViewById(R.id.text_address);
+        gochatButton = findViewById(R.id.button_gochat);
 
         nameTextView.setText(contact.getName());
         phoneTextView.setText(contact.getPhone());
         addressTextView.setText(contact.getAddress());
+
+        gochatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ChatActivity.class);
+
+                i.putExtra("contactDetails", contact);
+
+                startActivity(i);
+            }
+        });
 
 
     }
